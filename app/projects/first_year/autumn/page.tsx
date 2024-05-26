95f258033cddc14b98d33140368d34601e33b63a
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-26 11:17:58 +0300

    Update page.tsx

--- app/projects/first_year/autumn/page.tsx
+++ app/projects/first_year/autumn/page.tsx
@@ -1,4 +1,3 @@
-import example from './example_img.jpg';
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,19 +5,19 @@ export default function Page() {
   return (
     <div className="container">
       <h1 className="head">kiosk in school</h1>
-      <p className="description">
-        How I got to the project:&lt;br /&gt;
-        The reason I got to make this project was because I was interested in business and I knew it could teach me a lot and it could also make some money&lt;br /&gt;
-        &lt;br /&gt;
-        What I did:&lt;br /&gt;
-        I created a kiosk in school. Essentially I just sold cola and other light drinks and at the same time seeing where I could land higher sales and profit margins.
-      </p>
+      <p
+        className="description"
+        dangerouslySetInnerHTML={{
+          __html:
+            "How I got to the project:<br />The reason I got to make this project was because I was interested in business and I knew it could teach me a lot and it could also make some money<br /><br />What I did:<br />I created a kiosk in school. Essentially I just sold cola and other light drinks and at the same time seeing where I could land higher sales and profit margins.",
+        }}
+      />
       <div className="images">
         <div className="image-container">
-          <Image className="image" src={example} alt="" />
+          <Image className="image" src={require("./example_img.jpg")} alt="" />
         </div>
         <div className="image-container">
-          <Image className="image" src={example} alt="" />
+          <Image className="image" src={require("./example_img.jpg")} alt="" />
         </div>
         <ul className="linksprj">
           <li className="linkprj">
