772ed7b105e85e3c186db30ab482321993e688bf
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-26 11:25:01 +0300

    Update page.tsx

--- app/projects/second_year/winter/page.tsx
+++ app/projects/second_year/winter/page.tsx
@@ -1,18 +1,14 @@
 import example from './example_img.jpg';
-import Image from "next/image";
+import Image from "next/image"
 import Link from "next/link";
 
 export default function Page() {
   return (
     <div className="container">
       <h1 className="head">Stock algorithm</h1>
-      <p className="description">
-        How I got to the project:&lt;br /&gt;
-        Because my last project was basically doing the same thing just that now I had more knowledge I decided to keep going with this project.&lt;br /&gt;
-        &lt;br /&gt;
-        What I did:&lt;br /&gt;
-        I learned here a lot about matplotlib as well as plotting and although I didn&apos;t build an algorithm I improved much towards my goal and it also taught me how to plot stocks so I can also incorporate machine learning to it.
-      </p>
+      <p className="description" dangerouslySetInnerHTML={{
+        __html: "How I got to the project:<br />Because my last project was basically doing the same thing just that now I had more knowledge I decided to keep going with this project.<br /><br />What I did:<br />I learned here a lot about matplotlib as well as plotting and although I didn&apos;t build an algorithm I improved much towards my goal and it also taught me how to plot stocks so I can also incorporate machine learning to it."
+      }} />
       <div className="images">
         <div className="image-container">
           <Image className="image" src={example} alt="" />
