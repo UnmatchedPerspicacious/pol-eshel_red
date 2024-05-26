23ccc9e68398f23f0341a6f5649b6ce66b0c17a1
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-26 11:19:12 +0300

    Update page.tsx

--- app/projects/first_year/spring/page.tsx
+++ app/projects/first_year/spring/page.tsx
@@ -1,4 +1,3 @@
-import example from './example_img.jpg';
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,19 +5,19 @@ export default function Page() {
   return (
     <div className="container">
       <h1 className="head">Moveable plane in unity</h1>
-      <p className="description">
-        How I got to the project:&lt;br /&gt;
-        I actually saw ori someone in my class coding and because it seemed quite interesting I decided to start&lt;br /&gt;
-        &lt;br /&gt;
-        What I did:&lt;br /&gt;
-        Right here I started coding and I followed along a course that at the end tought me how to build a plane that can fly in x, y, z dimensions in unity
-      </p>
+      <p
+        className="description"
+        dangerouslySetInnerHTML={{
+          __html:
+            "How I got to the project:<br />I actually saw ori someone in my class coding and because it seemed quite interesting I decided to start<br /><br />What I did:<br />Right here I started coding and I followed along a course that at the end tought me how to build a plane that can fly in x, y, z dimensions in unity",
+        }}
+      />
       <div className="images">
         <div className="image-container">
-          <Image className="image" src={example} alt="" />
+          <Image className="image" src={require("./example_img.jpg")} alt="" />
         </div>
         <div className="image-container">
-          <Image className="image" src={example} alt="" />
+          <Image className="image" src={require("./example_img.jpg")} alt="" />
         </div>
         <ul className="linksprj">
           <li className="linkprj">
