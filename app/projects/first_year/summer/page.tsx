e77a72dc8ca24419b4a666b03610d54d685ed091
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-26 11:19:59 +0300

    Update page.tsx

--- app/projects/first_year/summer/page.tsx
+++ app/projects/first_year/summer/page.tsx
@@ -1,4 +1,3 @@
-import example from './example_img.jpg';
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,19 +5,19 @@ export default function Page() {
   return (
     <div className="container">
       <h1 className="head">Penguin game in unity</h1>
-      <p className="description">
-        How I got to the project:&lt;br /&gt;
-        I actually got to the project by ending my course in unity and then I decided to make a game with aviv a student from my class&lt;br /&gt;
-        &lt;br /&gt;
-        What I did:&lt;br /&gt;
-        Me and aviv someone in my class created a penguin game, where the penguin essentially tries to dodge objects flying from the sky, if I still have the project I&apos;ll let you play but I warn you it&apos;s quite bad &amp;#128517;.
-      </p>
+      <p
+        className="description"
+        dangerouslySetInnerHTML={{
+          __html:
+            "How I got to the project:<br />I actually got to the project by ending my course in unity and then I decided to make a game with aviv a student from my class<br /><br />What I did:<br />Me and aviv someone in my class created a penguin game, where the penguin essentially tries to dodge objects flying from the sky, if I still have the project I&apos;ll let you play but I warn you it&apos;s quite bad &#128517;.",
+        }}
+      />
       <div className="images">
         <div className="image-container">
-          <Image className="image" src={example} alt="" />
+          <Image className="image" src={require("./example_img.jpg")} alt="" />
         </div>
         <div className="image-container">
-          <Image className="image" src={example} alt="" />
+          <Image className="image" src={require("./example_img.jpg")} alt="" />
         </div>
         <ul className="linksprj">
           <li className="linkprj">
