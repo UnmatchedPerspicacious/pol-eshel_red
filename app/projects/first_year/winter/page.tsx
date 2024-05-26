aba071ce8037f612c07ce710d1f80162f16d0215
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-26 10:29:25 +0300

    Update page.tsx
    I did the same thing I did to the last page in this page as well

--- app/projects/first_year/winter/page.tsx
+++ app/projects/first_year/winter/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
       <h1 className="head">Food court in school</h1>
       <p className="description">
         How I got to the project:<br />
-        Unfortunately imri said that the kiosk I made wasn't ethical and wasn't necessarily legal so I had to shut it down.<br />
+        Unfortunately imri said that the kiosk I made wasn&apos;t ethical and wasn&apost necessarily legal so I had to shut it down.<br />
         <br />
         What I did:<br />
         This was a food court I made just that this time I also built a small space for it as well using argamans room and it sold hot dogs and chips.
