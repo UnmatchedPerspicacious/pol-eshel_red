a84f23b09277ee60e1211bf60afe2d7957d39b88
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-26 10:34:00 +0300

    Update page.tsx
    I'm just doing the exact same thing I did in the last two pages nothing special

--- app/projects/second_year/summer/page.tsx
+++ app/projects/second_year/summer/page.tsx
@@ -13,16 +13,16 @@ return <div className="container">
 <p className="description">
 
 How I got to the algo trading project:<br />
-Since I already started it in the past I kept going with it this time I was sure everything would be fine because I realised I could create a simple algorithm and then apply ml to it but guess what, the api I wanted to use was from age 18+, so I just added some line in pycharm which told me to keep grinding one the project in a year and half when I'm 18.<br />
+Since I already started it in the past I kept going with it this time I was sure everything would be fine because I realised I could create a simple algorithm and then apply ml to it but guess what, the api I wanted to use was from age 18+, so I just added some line in pycharm which told me to keep grinding on the project in a year and half when I&apos;m 18.<br />
 <br />
 How I got to the project tiod:<br />
 I actually got to this project because there was a hackathon and we needed to create some project that would help school and my friends and I thought it would be cool to create an application that would allow people to connect both in a friendly manner and professional mannner and because we so called "Won the hackathon", we got 3,000 nis for the project so we decided to keep working on it.<br />
 <br />
 What I did in the stock algorithm:<br />
-As for the stock algorithm your more than welcome to check my previous pages to see more of it however I'm fairly certain I'll just keep working on it when I'm 18 because of the age restriction of the api<br />
+As for the stock algorithm your more than welcome to check my previous pages to see more of it however I'm fairly certain I'll just keep working on it when I&apos;m 18 because of the age restriction of the api<br />
 <br />
-What I'm currently doing in tiod:<br />
-In tiod there are going to be multiple tasks my team and I must complete and the task I'm going to do is create the database using postgresql, and I'm probably going to do there other things as it advances 🗿
+What I&apos;m currently doing in tiod:<br />
+In tiod there are going to be multiple tasks my team and I must complete and the task I&apos;m going to do is create the database using postgresql, and I&apos;m probably going to do there other things as it advances 🗿
 
 </p>
 
