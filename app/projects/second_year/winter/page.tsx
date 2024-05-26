3f33e6a5c87a3a0fcd325604cdda01a1db87e45e
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-26 11:03:48 +0300

    Update page.tsx

--- app/projects/second_year/winter/page.tsx
+++ app/projects/second_year/winter/page.tsx
@@ -1,5 +1,5 @@
 import example from './example_img.jpg';
-import Image from "next/image"
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Page() {
@@ -7,10 +7,10 @@ export default function Page() {
     <div className="container">
       <h1 className="head">Stock algorithm</h1>
       <p className="description">
-        How I got to the project:<br />
-        Because my last project was basically doing the same thing just that now I had more knowledge I decided to keep going with this project.<br />
-        <br />
-        What I did:<br />
+        How I got to the project:&lt;br /&gt;
+        Because my last project was basically doing the same thing just that now I had more knowledge I decided to keep going with this project.&lt;br /&gt;
+        &lt;br /&gt;
+        What I did:&lt;br /&gt;
         I learned here a lot about matplotlib as well as plotting and although I didn&apos;t build an algorithm I improved much towards my goal and it also taught me how to plot stocks so I can also incorporate machine learning to it.
       </p>
       <div className="images">
@@ -24,8 +24,12 @@ export default function Page() {
           <li className="linkprj">
             <Link href={"#"}>LinkedIn</Link>
           </li>
-          <li className="linkprj"><Link href={"#"}>Github</Link></li>
-          <li className="linkprj"><Link href={"#"}>Facebook</Link></li>
+          <li className="linkprj">
+            <Link href={"#"}>Github</Link>
+          </li>
+          <li className="linkprj">
+            <Link href={"#"}>Facebook</Link>
+          </li>
         </ul>
       </div>
     </div>
