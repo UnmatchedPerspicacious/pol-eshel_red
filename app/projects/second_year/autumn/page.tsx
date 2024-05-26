26420c530f499bbf90d32dfeb5f7f021d7054a08
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-26 10:58:45 +0300

    Update page.tsx

--- app/projects/second_year/autumn/page.tsx
+++ app/projects/second_year/autumn/page.tsx
@@ -1,5 +1,5 @@
 import example from './example_img.jpg';
-import Image from "next/image"
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Page() {
@@ -7,10 +7,10 @@ export default function Page() {
     <div className="container">
       <h1 className="head">Attempt at creating a trading algorithm</h1>
       <p className="description">
-        How I got to the project:<br />
-        Since I realized unity is garbage and python is much better I learned python, I also got introduced into algo trading and then I decided to make this project<br />
-        <br />
-        What I did:<br />
+        How I got to the project:&lt;br /&gt;
+        Since I realized unity is garbage and python is much better I learned python, I also got introduced into algo trading and then I decided to make this project&lt;br /&gt;
+        &lt;br /&gt;
+        What I did:&lt;br /&gt;
         Essentially I got stock data through an api and tried to apply some functionality to it so it will end up as an algorithm.
       </p>
       <div className="images">
@@ -24,8 +24,12 @@ export default function Page() {
           <li className="linkprj">
             <Link href={"#"}>LinkedIn</Link>
           </li>
-          <li className="linkprj"><Link href={"#"}>Github</Link></li>
-          <li className="linkprj"><Link href={"#"}>Facebook</Link></li>
+          <li className="linkprj">
+            <Link href={"#"}>Github</Link>
+          </li>
+          <li className="linkprj">
+            <Link href={"#"}>Facebook</Link>
+          </li>
         </ul>
       </div>
     </div>
