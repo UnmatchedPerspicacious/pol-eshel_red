8a991104f170f0f47cbefdeed31a3d1c034d0251
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-26 10:59:36 +0300

    Update page.tsx

--- app/projects/second_year/spring/page.tsx
+++ app/projects/second_year/spring/page.tsx
@@ -1,5 +1,4 @@
-
-import Image from "next/image"
+import Image from "next/image";
 import Link from "next/link";
 import ml from './ml_trading.png';
 
@@ -8,11 +7,11 @@ export default function Page() {
     <div className="container">
       <h1 className="head">Ml stock prediction </h1>
       <p className="description">
-        How I got to the project:<br />
-        Basically because I already had the algo trading project I kept going for it just that now instead of thinking of it in a way which I first create the algorithm I thought of first creating a ml model which would also teach me a lot about it and would help my future algorithm learn<br />
-        <br />
-        What I did:<br />
-        I built a machine learning model and taught it to predict stocks open, high, low, volume, and close values and it actually got a very low loss function which is good because basically the lower it is the more accurate the model 🗿.
+        How I got to the project:&lt;br /&gt;
+        Basically because I already had the algo trading project I kept going for it just that now instead of thinking of it in a way which I first create the algorithm I thought of first creating a ml model which would also teach me a lot about it and would help my future algorithm learn&lt;br /&gt;
+        &lt;br /&gt;
+        What I did:&lt;br /&gt;
+        I built a machine learning model and taught it to predict stocks open, high, low, volume, and close values and it actually got a very low loss function which is good because basically the lower it is the more accurate the model &amp;rang;.
       </p>
       <div className="images">
         <div className="image-container">
