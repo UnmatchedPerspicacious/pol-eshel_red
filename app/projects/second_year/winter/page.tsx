eee689db621e8557c0e8e859a99f094d4ce4be36
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-26 11:32:51 +0300

    Update page.tsx

--- app/projects/second_year/winter/page.tsx
+++ app/projects/second_year/winter/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     <div className="container">
       <h1 className="head">Stock algorithm</h1>
       <p className="description" dangerouslySetInnerHTML={{
-        __html: "How I got to the project:<br />Because my last project was basically doing the same thing just that now I had more knowledge I decided to keep going with this project.<br /><br />What I did:<br />I learned here a lot about matplotlib as well as plotting and although I didn&apos;t build an algorithm I improved much towards my goal and it also taught me how to plot stocks so I can also incorporate machine learning to it."
+        __html: "How I got to the project:Because my last project was basically doing the same thing just that now I had more knowledge I decided to keep going with this project.What I did:I learned here a lot about matplotlib as well as plotting and although I didn&apos;t build an algorithm I improved much towards my goal and it also taught me how to plot stocks so I can also incorporate machine learning to it."
       }} />
       <div className="images">
         <div className="image-container">
