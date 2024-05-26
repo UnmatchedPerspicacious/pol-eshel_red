12351fb34cbb72717776ddebe82f3122546cf1c6
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-26 11:36:47 +0300

    Update page.tsx

--- app/projects/second_year/spring/page.tsx
+++ app/projects/second_year/spring/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
         className="description"
         dangerouslySetInnerHTML={{
           __html:
-            "How I got to the project:<br />Basically because I already had the algo trading project I kept going for it just that now instead of thinking of it in a way which I first create the algorithm I thought of first creating a ml model which would also teach me a lot about it and would help my future algorithm learn<br /><br />What I did:<br />I built a machine learning model and taught it to predict stocks open, high, low, volume, and close values and it actually got a very low loss function which is good because basically the lower it is the more accurate the model &rang;.",
+            "How I got to the project:Basically because I already had the algo trading project I kept going for it just that now instead of thinking of it in a way which I first create the algorithm I thought of first creating a ml model which would also teach me a lot about it and would help my future algorithm learn What I did:I built a machine learning model and taught it to predict stocks open, high, low, volume, and close values and it actually got a very low loss function which is good because basically the lower it is the more accurate the model &rang;.",
         }}
       />
       <div className="images">
