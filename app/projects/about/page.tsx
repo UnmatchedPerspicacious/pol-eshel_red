8e0bc92e40dcfa4a0165864c25a794f30c2a01ac
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-26 10:54:02 +0300

    Update page.tsx
    For the next few files I'm going to do the same thing  I did here which is fixing it so that vercel will manage to deploy it. So there is nothing special about these commits

--- app/projects/about/page.tsx
+++ app/projects/about/page.tsx
@@ -1,31 +1,34 @@
 import Link from "next/link";
 
-
-export default function socials(){
-  
-  return <div className="container">
-    <h1 className="head">About me</h1>
-    <div className="descriptions">
-    <div className="aboutme">
-     <h1 className="whoami">Who am i?</h1>
-    <p className="description">
-        My name is Eshel Nagar. Fitness, chess, coding, and finances - these started as hobbies, but they&apos;re slowly becoming professions.  I haven&apos;t uploaded anything to LinkedIn yet, but I have uploaded a thing or two to GitHub. I do frequently post on Instagram. (You can find the links below👇.)
-    </p>
+export default function socials() {
+  return (
+    <div className="container">
+      <h1 className="head">About me</h1>
+      <div className="descriptions">
+        <div className="aboutme">
+          <h1 className="whoami">Who am i?</h1>
+          <p className="description">
+            My name is Eshel Nagar. Fitness, chess, coding, and finances - these started as hobbies, but they&apos;re slowly becoming professions. I haven&apos;t uploaded anything to LinkedIn yet, but I have uploaded a thing or two to GitHub. I do frequently post on Instagram. (You can find the links below&darr;.)
+          </p>
+        </div>
+        <div className="CV">
+          <h1 className="CV-head">CV</h1>
+          <p className="CV-text">
+            At this school, I&apos;m majoring in computer science with a focus on algorithmic trading. My achievements include developing programming projects and gaining knowledge of cryptocurrency. In the future, I aim to create a lucrative stock-trading algorithm that incorporates machine learning. Additionally, I&apos;m working on a groundbreaking application called Tiod with my team. You&apos;ll be hearing more about it soon!&rang;
+          </p>
+        </div>
+      </div>
+      <ul className="links">
+        <li className="link">
+          <Link href={"https://www.linkedin.com/feed/"}>LinkedIn</Link>
+        </li>
+        <li className="link">
+          <Link href={"https://github.com/UnmatchedPerspicacious/UnmatchedPerspicacious"}>Github</Link>
+        </li>
+        <li className="link">
+          <Link href={"https://www.instagram.com/eshesthenics/"}>Instagram</Link>
+        </li>
+      </ul>
     </div>
-    <div className="CV">
-      <h1 className="CV-head">CV</h1>
-      <p className="CV-text">
-         At this school, I&apos;m majoring in computer science with a focus on algorithmic trading. My achievements include developing programming projects and gaining knowledge of cryptocurrency. In the future, I aim to create a lucrative stock-trading algorithm that incorporates machine learning. Additionally, I&apos;m working on a groundbreaking application called Tiod with my team. You&apos;ll be hearing more about it soon!🗿
-      </p>
-    </div>
-    </div>
-    <ul className="links">
-  
-      <li className="link">
-        <Link href={"https://www.linkedin.com/feed/"}>LinkedIn</Link>
-      </li>
-      <li className="link"><Link href={"https://github.com/UnmatchedPerspicacious/UnmatchedPerspicacious"}>Github</Link></li>
-      <li className="link"><Link href={"https://www.instagram.com/eshesthenics/"}>Instagram</Link></li>
-    </ul>
-    </div>
+  );
 }
