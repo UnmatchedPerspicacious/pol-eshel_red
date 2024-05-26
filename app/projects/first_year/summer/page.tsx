1c179dae1144a788cf2113cac339d7376485ea43
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-26 10:58:01 +0300

    Update page.tsx

--- app/projects/first_year/summer/page.tsx
+++ app/projects/first_year/summer/page.tsx
@@ -1,5 +1,5 @@
 import example from './example_img.jpg';
-import Image from "next/image"
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Page() {
@@ -7,11 +7,11 @@ export default function Page() {
     <div className="container">
       <h1 className="head">Penguin game in unity</h1>
       <p className="description">
-        How I got to the project:<br />
-        I actually got to the project by ending my course in unity and then I decided to make a game with aviv a student from my class<br />
-        <br />
-        What I did:<br />
-        Me and aviv someone in my class created a penguin game, where the penguin essentially tries to dodge objects flying from the sky, if I still have the project I&apos;ll let you play but I warn you it&apos;s quite bad 🤣.
+        How I got to the project:&lt;br /&gt;
+        I actually got to the project by ending my course in unity and then I decided to make a game with aviv a student from my class&lt;br /&gt;
+        &lt;br /&gt;
+        What I did:&lt;br /&gt;
+        Me and aviv someone in my class created a penguin game, where the penguin essentially tries to dodge objects flying from the sky, if I still have the project I&apos;ll let you play but I warn you it&apos;s quite bad &amp;#128517;.
       </p>
       <div className="images">
         <div className="image-container">
@@ -24,8 +24,12 @@ export default function Page() {
           <li className="linkprj">
             <Link href={"#"}>LinkedIn</Link>
           </li>
-          <li className="linkprj"><Link href={"#"}>Github</Link></li>
-          <li className="linkprj"><Link href={"#"}>Facebook</Link></li>
+          <li className="linkprj">
+            <Link href={"#"}>Github</Link>
+          </li>
+          <li className="linkprj">
+            <Link href={"#"}>Facebook</Link>
+          </li>
         </ul>
       </div>
     </div>
