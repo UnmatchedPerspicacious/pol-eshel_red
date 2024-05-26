fea8d37ba9d261ce5105fa0c0c929720119deb7f
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-26 11:22:18 +0300

    Update page.tsx

--- app/projects/second_year/autumn/page.tsx
+++ app/projects/second_year/autumn/page.tsx
@@ -6,13 +6,9 @@ export default function Page() {
   return (
     <div className="container">
       <h1 className="head">Attempt at creating a trading algorithm</h1>
-      <p className="description">
-        How I got to the project:&lt;br /&gt;
-        Since I realized unity is garbage and python is much better I learned python, I also got introduced into algo trading and then I decided to make this project&lt;br /&gt;
-        &lt;br /&gt;
-        What I did:&lt;br /&gt;
-        Essentially I got stock data through an api and tried to apply some functionality to it so it will end up as an algorithm.
-      </p>
+      <p className="description" dangerouslySetInnerHTML={{
+        __html: "How I got to the project:<br />Since I realized unity is garbage and python is much better I learned python, I also got introduced into algo trading and then I decided to make this project<br /><br />What I did:<br />Essentially I got stock data through an api and tried to apply some functionality to it so it will end up as an algorithm."
+      }} />
       <div className="images">
         <div className="image-container">
           <Image className="image" src={example} alt="" />
