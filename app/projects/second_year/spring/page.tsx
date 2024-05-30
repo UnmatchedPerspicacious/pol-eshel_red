13f8029ad363068944be39bf3fc0bb307fa5c8ed
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-30 19:08:51 +0300

    Update page.tsx

--- app/projects/second_year/spring/page.tsx
+++ app/projects/second_year/spring/page.tsx
@@ -32,7 +32,7 @@ export default function Page() {
               <Link href="/">Github</Link> 
             </li>
             <li className="linkprj">
-              <Link href="/">Facebook</Link> 
+              <Link href="/">Instagram</Link> 
             </li>
           </ul>
         </div>
