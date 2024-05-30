50d17957034af9b13e9a1c67fc34ef3005bbe0fb
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-30 18:38:40 +0300

    Update page.tsx
    Made there not be any image examples because i have no images, also fixed previous bugs

--- app/projects/second_year/winter/page.tsx
+++ app/projects/second_year/winter/page.tsx
@@ -6,16 +6,15 @@ export default function Page() {
   return (
     <div className="container">
       <h1 className="head">Stock algorithm</h1>
-      <p className="description" dangerouslySetInnerHTML={{
-        __html: "How I got to the project:Because my last project was basically doing the same thing just that now I had more knowledge I decided to keep going with this project.What I did:I learned here a lot about matplotlib as well as plotting and although I didn&apos;t build an algorithm I improved much towards my goal and it also taught me how to plot stocks so I can also incorporate machine learning to it."
-      }} />
-      <div className="images">
-        <div className="image-container">
-          <Image className="image" src={example} alt="" />
-        </div>
-        <div className="image-container">
-          <Image className="image" src={example} alt="" />
-        </div>
+      <p className="description"/>
+        How I got to the project:
+        <br/>
+        Because my last project was basically doing the same thing just that now I had more knowledge I decided to keep going with this project.
+        <br/>
+        What I did:
+        <br/>
+        I learned here a lot about matplotlib as well as plotting and although I didn&apos;t build an algorithm I improved much towards my goal and it also taught me how to plot stocks so I can also incorporate machine learning to it.
+      <p/>
         <ul className="linksprj">
           <li className="linkprj">
             <Link href={"#"}>LinkedIn</Link>
@@ -27,7 +26,6 @@ export default function Page() {
             <Link href={"#"}>Facebook</Link>
           </li>
         </ul>
-      </div>
     </div>
   );
 }
