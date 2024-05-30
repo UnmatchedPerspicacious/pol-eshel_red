c09755a1c245e91e9f40aeb6ac71b12183ba3977
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-30 18:59:14 +0300

    Update page.tsx
    added br statements

--- app/projects/second_year/spring/page.tsx
+++ app/projects/second_year/spring/page.tsx
@@ -8,12 +8,12 @@ export default function Page() {
     <div className="container">
       <h1 className="head">ML Stock Prediction</h1> 
       <p className="description">
-        **How I got to the project:**
-
+        How I got to the project:
+        <br/>
         Basically, I already had the algo trading project going, but I decided to take a different approach.  Instead of creating the algorithm first, I thought it would be beneficial to build a machine learning model first. This would not only help me learn more about ML, but also potentially improve my future algorithm.
-
-        **What I did:**
-
+        <br/>
+        What I did:
+        <br/>
         I built a machine learning model and trained it to predict stock open, high, low, volume, and close values. It achieved a very low loss function, which is excellent because a lower loss function indicates a more accurate model.
       </p>
       <div className="images">
