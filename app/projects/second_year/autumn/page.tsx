48deab29f9cba3027d05cdc346f9db7d8e006d6e
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-30 17:40:38 +0300

    Update page.tsx

--- app/projects/second_year/autumn/page.tsx
+++ app/projects/second_year/autumn/page.tsx
@@ -1,21 +1,17 @@
-import example from './example_img.jpg';
-import Image from "next/image";
-import Link from "next/link";
-
 export default function Page() {
   return (
     <div className="container">
       <h1 className="head">Attempt at creating a trading algorithm</h1>
-      <p className="description" dangerouslySetInnerHTML={{
-        __html: "How I got to the project: Since I realized unity is garbage and python is much better I learned python, I also got introduced into algo trading and then I decided to make this project What I did: Essentially I got stock data through an api and tried to apply some functionality to it so it will end up as an algorithm."
-      }} />
-      <div className="images">
-        <div className="image-container">
-          <Image className="image" src={example} alt="" />
+      <p className="description">
+        How I got to the project: 
+        <br/>
+        Since I realized unity is garbage and python is much better I learned python, I also got introduced into algo trading and then I decided to make this project
+        <br/>
+        What I did: 
+        <br/>
+        Essentially I got stock data through an api and tried to apply some functionality to it so it will end up as an algorithm.
         </div>
-        <div className="image-container">
-          <Image className="image" src={example} alt="" />
-        </div>
+
         <ul className="linksprj">
           <li className="linkprj">
             <Link href={"#"}>LinkedIn</Link>
@@ -27,7 +23,8 @@ export default function Page() {
             <Link href={"#"}>Facebook</Link>
           </li>
         </ul>
-      </div>
+        </div>
+      
     </div>
   );
 }
