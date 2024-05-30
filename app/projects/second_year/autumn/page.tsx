06e439cac89e7aaa8a373155b119f4751138ea81
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-30 18:49:30 +0300

    Update page.tsx
    I essentially just changed the code to the same format it is in winter second year and hopefully everything should compile well

--- app/projects/second_year/autumn/page.tsx
+++ app/projects/second_year/autumn/page.tsx
@@ -1,17 +1,21 @@
+import example from './example_img.jpg';
+import Image from "next/image"
+import Link from "next/link";
+
+
 export default function Page() {
   return (
     <div className="container">
-      <h1 className="head">Attempt at creating a trading algorithm</h1>
-      <p className="description">
-        How I got to the project: 
+      <h1 className="head">ttempt at creating a trading algorithm</h1>
+      <p className="description"/>
+        How I got to the project:
         <br/>
         Since I realized unity is garbage and python is much better I learned python, I also got introduced into algo trading and then I decided to make this project
         <br/>
-        What I did: 
+        What I did:
         <br/>
         Essentially I got stock data through an api and tried to apply some functionality to it so it will end up as an algorithm.
-        </div>
-
+      <p/>
         <ul className="linksprj">
           <li className="linkprj">
             <Link href={"#"}>LinkedIn</Link>
@@ -23,8 +27,6 @@ export default function Page() {
             <Link href={"#"}>Facebook</Link>
           </li>
         </ul>
-        </div>
-      
     </div>
   );
 }
