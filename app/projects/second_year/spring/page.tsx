3b4c6dd91d751566bf627c205b6430dbcc90f1ed
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-30 11:21:48 +0300

    Update page.tsx
    Just fixed some errors hopefully this will compile successfully

--- app/projects/second_year/spring/page.tsx
+++ app/projects/second_year/spring/page.tsx
@@ -4,18 +4,19 @@ import ml from "./ml_trading.png";
 
 export default function Page() {
   return (
-    <div className="container"> </div>
-      <h1 className="head">Ml stock prediction </h1>
-    );
-  }
-      <p
-        className="description">
-            "How I got to the project:Basically because I already had the algo trading project I kept going for it just that now instead of thinking of it in a way which I first create the algorithm I thought of first creating a ml model which would also teach me a lot about it and would help my future algorithm learn What I did:I built a machine learning model and taught it to predict stocks open, high, low, volume, and close values and it actually got a very low loss function which is good because basically the lower it is the more accurate the model &rang;.",
-    
-      /p>
+    <div className="container">
+      <h1 className="head">Ml stock prediction</h1>
+      <p className="description">
+        "How I got to the project:Basically because I already had the algo trading project I kept going for it just that now instead of thinking of it in a way which I first create the algorithm I thought of first creating a ml model which would also teach me a lot about it and would help my future algorithm learn What I did:I built a machine learning model and taught it to predict stocks open, high, low, volume, and close values and it actually got a very low loss function which is good because basically the lower it is the more accurate the model &rang;.",
+      </p>
       <div className="images">
         <div className="image-container">
-          <Image className="image" src={ml} alt="" /> </div>
+          <Image className="image" src={ml} alt="" />
         </div>
-        <div className="image-container">
-          <Image className="image" src={require("./example
+      </div>
+      <div className="image-container">
+        <Image className="image" src={require("./example")} alt="" />
+      </div>
+    </div>
+  );
+}
