80b357cb0f89765c9ce9f06bfe4ee5a70d3465aa
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-30 17:03:01 +0300

    Update page.tsx
    fixing other bugs

--- app/projects/second_year/spring/page.tsx
+++ app/projects/second_year/spring/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+
 import ml from "./ml_trading.png";
 
 export default function Page() {
@@ -7,23 +8,23 @@ export default function Page() {
     <div className="container">
       <h1 className="head">Ml stock prediction</h1>
       <p className="description">
-        "How I got to the project: Basically because I already had the algo
+        &quot;How I got to the project: Basically because I already had the algo
         trading project I kept going for it just that now instead of thinking
         of it in a way which I first create the algorithm I thought of first
         creating a ml model which would also teach me a lot about it and would
         help my future algorithm learn. What I did: I built a machine learning
         model and taught it to predict stocks open, high, low, volume, and
         close values and it actually got a very low loss function which is
-        good because basically the lower it is the more accurate the model &rang;."
+        good because basically the lower it is the more accurate the model &rang;.&quot;
       </p>
       <div className="images">
         <div className="image-container">
-          {/* Use standard img tag for local images */}
-          <img className="image" src={ml} alt="ML stock prediction" />
-        </div>
-        <div className="image-container">
-          {/* Use standard img tag for local images */}
-          <img className="image" src={ml} alt="ML stock prediction" />
+          <Image
+            src={ml}
+            alt="ML stock prediction"
+            width={450}
+            height={300} 
+          />
         </div>
       </div>
     </div>
