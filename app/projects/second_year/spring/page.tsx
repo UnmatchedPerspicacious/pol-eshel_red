72be8ed5e47229411d056f40d3b1ae6d1f425228
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-30 18:55:11 +0300

    Update page.tsx
    gemini please save us

--- app/projects/second_year/spring/page.tsx
+++ app/projects/second_year/spring/page.tsx
@@ -1,47 +1,40 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
-import ml from "./ml_trading.png";
+import mlImage from './ml_trading.png'; 
 
 export default function Page() {
   return (
-    <className="container">
-      <h1 className="head">Ml stock prediction</h1>
+    <div className="container">
+      <h1 className="head">ML Stock Prediction</h1> 
       <p className="description">
-        How I got to the project: 
-        <br/>
-        Basically because I already had the algo
-        trading project I kept going for it just that now instead of thinking
-        of it in a way which I first create the algorithm I thought of first
-        creating a ml model which would also teach me a lot about it and would
-        help my future algorithm learn.
-        <br/>
-        What I did: 
-        <br/>
-        I built a machine learning
-        model and taught it to predict stocks open, high, low, volume, and
-        close values and it actually got a very low loss function which is
-        good because basically the lower it is the more accurate the model
+        **How I got to the project:**
+
+        Basically, I already had the algo trading project going, but I decided to take a different approach.  Instead of creating the algorithm first, I thought it would be beneficial to build a machine learning model first. This would not only help me learn more about ML, but also potentially improve my future algorithm.
+
+        **What I did:**
+
+        I built a machine learning model and trained it to predict stock open, high, low, volume, and close values. It achieved a very low loss function, which is excellent because a lower loss function indicates a more accurate model.
       </p>
       <div className="images">
         <div className="image-container">
           <Image
-            src={ml}
-            alt="ML stock prediction"
+            src={mlImage}
+            alt="ML Stock Prediction"
             width={450}
-            height={300} 
-                    <ul className="linksprj">
-          <li className="linkprj">
-            <Link href={"#"}>LinkedIn</Link>
-          </li>
-          <li className="linkprj">
-            <Link href={"#"}>Github</Link>
-          </li>
-          <li className="linkprj">
-            <Link href={"#"}>Facebook</Link>
-          </li>
-        </ul>
+            height={300}
           />
+          <ul className="linksprj">
+            <li className="linkprj">
+              <Link href="/">LinkedIn</Link> 
+            </li>
+            <li className="linkprj">
+              <Link href="/">Github</Link> 
+            </li>
+            <li className="linkprj">
+              <Link href="/">Facebook</Link> 
+            </li>
+          </ul>
         </div>
       </div>
     </div>
