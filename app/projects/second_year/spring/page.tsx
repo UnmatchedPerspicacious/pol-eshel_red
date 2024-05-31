49f4ef61ed0251cd7e47ed034a34e049bcfa5912
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-05-31 11:07:13 +0300

    Update page.tsx

--- app/projects/second_year/spring/page.tsx
+++ app/projects/second_year/spring/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
           />
           <ul className="linksprj">
             <li className="linkprj">
-              <Link href="/">LinkedIn</Link> 
+              <Link href="https://www.linkedin.com/feed/">LinkedIn</Link> 
             </li>
             <li className="linkprj">
               <Link href="/">Github</Link> 
