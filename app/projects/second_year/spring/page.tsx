4ce9ff6e502da6f892cf5b759cf3905c65269795
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-06-01 18:32:36 +0300

    Update page.tsx
    there was some problem in the application where you couldn't see the Instagram button because it was to much to the right so hopefully this should fix that issue

--- app/projects/second_year/spring/page.tsx
+++ app/projects/second_year/spring/page.tsx
@@ -1,19 +1,19 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import mlImage from './ml_trading.png'; 
+import mlImage from './ml_trading.png';
 
 export default function Page() {
   return (
     <div className="container">
-      <h1 className="head">ML Stock Prediction</h1> 
+      <h1 className="head">ML Stock Prediction</h1>
       <p className="description">
         How I got to the project:
-        <br/>
-        Basically, I already had the algo trading project going, but I decided to take a different approach.  Instead of creating the algorithm first, I thought it would be beneficial to build a machine learning model first. This would not only help me learn more about ML, but also potentially improve my future algorithm.
-        <br/>
+        <br />
+        Basically, I already had the algo trading project going, but I decided to take a different approach. Instead of creating the algorithm first, I thought it would be beneficial to build a machine learning model first. This would not only help me learn more about ML, but also potentially improve my future algorithm.
+        <br />
         What I did:
-        <br/>
+        <br />
         I built a machine learning model and trained it to predict stock open, high, low, volume, and close values. It achieved a very low loss function, which is excellent because a lower loss function indicates a more accurate model.
       </p>
       <div className="images">
@@ -26,17 +26,48 @@ export default function Page() {
           />
           <ul className="linksprj">
             <li className="linkprj">
-              <Link href="https://www.linkedin.com/feed/">LinkedIn</Link> 
+              <Link href="https://www.linkedin.com/feed/">LinkedIn</Link>
             </li>
             <li className="linkprj">
-              <Link href="https://github.com/UnmatchedPerspicacious">Github</Link> 
+              <Link href="https://github.com/UnmatchedPerspicacious">Github</Link>
             </li>
             <li className="linkprj">
-              <Link href="https://www.instagram.com/eshesthenics/">Instagram</Link> 
+              <Link href="https://www.instagram.com/eshesthenics/">Instagram</Link>
             </li>
           </ul>
         </div>
       </div>
     </div>
   );
+}
+/* Styles for container */
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+/* Styles for image container */
+.image-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+/* Styles for links */
+.linksprj {
+  list-style-type: none;
+  padding: 0;
+  margin: 10px 0;
+  display: flex;
+  justify-content: center;
+}
+
+.linkprj {
+  margin: 0 10px;
+}
+
+.linkprj a {
+  text-decoration: none;
+  color: inherit;
 }
