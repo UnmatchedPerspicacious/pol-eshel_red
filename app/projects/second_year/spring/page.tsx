0ecd67869b8c572b84fb54135aab7c66aa7a4cfc
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-06-02 10:24:34 +0300

    Update page.tsx
    added br statements

--- app/projects/second_year/spring/page.tsx
+++ app/projects/second_year/spring/page.tsx
@@ -8,9 +8,11 @@ export default function Page() {
       <h1 className="head">Ml stock prediction </h1>
       <p className="description">
         How I got to the project:
+        <br />
         Basically because I already had the algo trading project I kept going for it just that now instead of thinking of it in a way which I first create the algorithm I thought of first creating a ml model which would also teach me a lot about it and would help my future algorithm learn
-
+        <br />
         What I did:
+        <br />
         I built a machine learning model and taught it to predict stocks open, high, low, volume, and close values and it actually got a very low loss function which is good because basically the lower it is the more accurate the model 🗿.
       </p>
       <div className="images">
