9438eb6ad65f66eb6fde963ea3773d6b82989cab
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-06-02 10:36:51 +0300

    Update page.tsx
    changed it to the format it is on my localhost

--- app/projects/second_year/winter/page.tsx
+++ app/projects/second_year/winter/page.tsx
@@ -1,32 +1,35 @@
 import example from './example_img.jpg';
 import Image from "next/image"
 import Link from "next/link";
-import React from 'react';
 
 export default function Page() {
   return (
     <div className="container">
       <h1 className="head">Stock algorithm</h1>
-      <p className="description"/>
+      <p className="description">
         How I got to the project:
-        <br/>
+        <br />
         Because my last project was basically doing the same thing just that now I had more knowledge I decided to keep going with this project.
-        <br/>
+        <br />
         What I did:
-        <br/>
+        <br />
         I learned here a lot about matplotlib as well as plotting and although I didn&apos;t build an algorithm I improved much towards my goal and it also taught me how to plot stocks so I can also incorporate machine learning to it.
-      <p/>
+      </p>
+      <div className="images">
+        <div className="image-container">
+          <Image className="image" src={example} alt="" />
+        </div>
+        <div className="image-container">
+          <Image className="image" src={example} alt="" />
+        </div>
         <ul className="linksprj">
           <li className="linkprj">
             <Link href={"https://www.linkedin.com/feed/"}>LinkedIn</Link>
           </li>
-          <li className="linkprj">
-            <Link href={"https://github.com/UnmatchedPerspicacious"}>Github</Link>
-          </li>
-          <li className="linkprj">
-            <Link href={"https://www.instagram.com/eshesthenics/"}>Instagram</Link>
-          </li>
+          <li className="linkprj"><Link href={"https://github.com/UnmatchedPerspicacious"}>Github</Link></li>
+          <li className="linkprj"><Link href={"https://www.instagram.com/eshesthenics/"}>Instagram</Link></li>
         </ul>
+      </div>
     </div>
   );
 }
