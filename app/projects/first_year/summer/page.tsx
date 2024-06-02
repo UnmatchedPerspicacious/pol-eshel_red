073d111fba5b9352a67c03f1a5e1725a285bd14a
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-06-02 10:57:03 +0300

    Update page.tsx
    Changed it to the format it is on my localhost

--- app/projects/first_year/summer/page.tsx
+++ app/projects/first_year/summer/page.tsx
@@ -1,34 +1,31 @@
-import Image from "next/image";
+import example from './example_img.jpg';
+import Image from "next/image"
 import Link from "next/link";
-import React from 'react';
+
 export default function Page() {
   return (
     <div className="container">
       <h1 className="head">Penguin game in unity</h1>
-      <p
-        className="description"
-        dangerouslySetInnerHTML={{
-          __html:
-            "How I got to the project:<br />I actually got to the project by ending my course in unity and then I decided to make a game with aviv a student from my class<br /><br />What I did:<br />Me and aviv someone in my class created a penguin game, where the penguin essentially tries to dodge objects flying from the sky, if I still have the project I&apos;ll let you play but I warn you it&apos;s quite bad &#128517;.",
-        }}
-      />
+      <p className="description">
+        How I got to the project:<br />
+        I actually got to the project by ending my course in unity and then I decided to make a game with aviv a student from my class<br />
+        <br />
+        What I did:<br />
+        Me and aviv someone in my class created a penguin game, where the penguin essentially tries to dodge objects flying from the sky, if I still have the project Ill let you play but I warn you it&apos;s quite bad 🤣.
+      </p>
       <div className="images">
         <div className="image-container">
-          <Image className="image" src={require("./example_img.jpg")} alt="" />
+          <Image className="image" src={example} alt="" />
         </div>
         <div className="image-container">
-          <Image className="image" src={require("./example_img.jpg")} alt="" />
+          <Image className="image" src={example} alt="" />
         </div>
         <ul className="linksprj">
           <li className="linkprj">
-            <Link href={"https://www.linkedin.com/feed/"}>LinkedIn</Link>
+            <Link href={"https://www.linkedin.com/in/eshel-nagar-3a8294259/"}>LinkedIn</Link>
           </li>
-          <li className="linkprj">
-            <Link href={"https://github.com/UnmatchedPerspicacious"}>Github</Link>
-          </li>
-          <li className="linkprj">
-            <Link href={"https://www.instagram.com/eshesthenics/"}>Instagram</Link>
-          </li>
+          <li className="linkprj"><Link href={"https://github.com/UnmatchedPerspicacious"}>Github</Link></li>
+          <li className="linkprj"><Link href={"https://www.instagram.com/eshesthenics/"}>Instagram</Link></li>
         </ul>
       </div>
     </div>
