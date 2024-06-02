e71314f90efc4f8a8b315c0ec02b33b677415bf3
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-06-02 10:31:50 +0300

    Update page.tsx
    added br statement for aesthetics

--- app/projects/second_year/spring/page.tsx
+++ app/projects/second_year/spring/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
       <p className="description">
         How I got to the project:
         <br />
-        Basically because I already had the algo trading project I kept going for it just that now instead of thinking of it in a way which I first create the algorithm I thought of first creating a ml model which would also teach me a lot about it and would help my future algorithm learn
+        Basically because I already had the algo trading project I kept going for it just that now instead of thinking of it in a way which I first create the algorithm I thought of first creating a ml model which would also teach me a lot about it and would help my future algorithm learn.<br /> 
         <br />
         What I did:
         <br />
