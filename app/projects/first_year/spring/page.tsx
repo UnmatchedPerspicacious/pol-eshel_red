d74f4e5d5fa84bf1bc9d33071fa15593a85bfc36
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-06-02 10:54:18 +0300

    Update page.tsx
    Changed it to the format it is on my localhost

--- app/projects/first_year/spring/page.tsx
+++ app/projects/first_year/spring/page.tsx
@@ -1,36 +1,34 @@
-import Image from "next/image";
+import example from './example_img.jpg';
+import Image from "next/image"
 import Link from "next/link";
-import React from 'react';
+
 export default function Page() {
   return (
     <div className="container">
       <h1 className="head">Moveable plane in unity</h1>
-      <p
-        className="description"
-        dangerouslySetInnerHTML={{
-          __html:
-            "How I got to the project:<br />I actually saw ori someone in my class coding and because it seemed quite interesting I decided to start<br /><br />What I did:<br />Right here I started coding and I followed along a course that at the end tought me how to build a plane that can fly in x, y, z dimensions in unity",
-        }}
-      />
+      <p className="description">
+        How I got to the project:<br />
+        I actually saw ori someone in my class coding and because it seemed quite interesting I decided to start<br />
+        <br />
+        What I did:<br />
+        Right here I started coding and I followed along a course that at the end tought me how to build a plane that can fly in x, y, z dimensions in unity
+      </p>
       <div className="images">
         <div className="image-container">
-          <Image className="image" src={require("./example_img.jpg")} alt="" />
+          <Image className="image" src={example} alt="" />
         </div>
         <div className="image-container">
-          <Image className="image" src={require("./example_img.jpg")} alt="" />
+          <Image className="image" src={example} alt="" />
         </div>
         <ul className="linksprj">
           <li className="linkprj">
-            <Link href={"https://www.linkedin.com/feed/"}>LinkedIn</Link>
+            <Link href={"www.linkedin.com/in/eshel-nagar-3a8294259/"}>LinkedIn</Link>
           </li>
-          <li className="linkprj">
-            <Link href={"https://github.com/UnmatchedPerspicacious"}>Github</Link>
-          </li>
-          <li className="linkprj">
-            <Link href={"https://www.instagram.com/eshesthenics/"}>Instagram</Link>
-          </li>
+          <li className="linkprj"><Link href={"https://github.com/UnmatchedPerspicacious"}>Github</Link></li>
+          <li className="linkprj"><Link href={"https://www.instagram.com/eshesthenics/"}>Instagram</Link></li>
         </ul>
       </div>
     </div>
   );
 }
+
