78ff38f20484e503c8e876fe950f6b54a76080f3
UnmatchedPerspicacious <157276646+UnmatchedPerspicacious@users.noreply.github.com>
2024-06-02 10:50:25 +0300

    Update page.tsx
    Changed it to the format it is on my localhost

--- app/projects/first_year/autumn/page.tsx
+++ app/projects/first_year/autumn/page.tsx
@@ -1,34 +1,31 @@
-import Image from "next/image";
+import example from './example_img.jpg';
+import Image from "next/image"
 import Link from "next/link";
-import React from 'react';
+
 export default function Page() {
   return (
     <div className="container">
       <h1 className="head">kiosk in school</h1>
-      <p
-        className="description"
-        dangerouslySetInnerHTML={{
-          __html:
-            "How I got to the project:<br />The reason I got to make this project was because I was interested in business and I knew it could teach me a lot and it could also make some money<br /><br />What I did:<br />I created a kiosk in school. Essentially I just sold cola and other light drinks and at the same time seeing where I could land higher sales and profit margins.",
-        }}
-      />
+      <p className="description">
+        How I got to the project:<br />
+        The reason I got to make this project was because I was interested in business and I knew it could teach me a lot and it could also make some money<br />
+        <br />
+        What I did:<br />
+        I created a kiosk in school. Essentially I just sold cola and other light drinks and at the same time seeing where I could land higher sales and profit margins.
+      </p>
       <div className="images">
         <div className="image-container">
-          <Image className="image" src={require("./example_img.jpg")} alt="" />
+          <Image className="image" src={example} alt="" />
         </div>
         <div className="image-container">
-          <Image className="image" src={require("./example_img.jpg")} alt="" />
+          <Image className="image" src={example} alt="" />
         </div>
         <ul className="linksprj">
           <li className="linkprj">
             <Link href={"https://www.linkedin.com/in/eshel-nagar-3a8294259/"}>LinkedIn</Link>
           </li>
-          <li className="linkprj">
-            <Link href={"https://github.com/UnmatchedPerspicacious"}>Github</Link>
-          </li>
-          <li className="linkprj">
-            <Link href={"https://www.instagram.com/eshesthenics/"}>Instagram</Link>
-          </li>
+          <li className="linkprj"><Link href={"https://github.com/UnmatchedPerspicacious"}>Github</Link></li>
+          <li className="linkprj"><Link href={"https://www.instagram.com/eshesthenics/"}>Instagram</Link></li>
         </ul>
       </div>
     </div>
